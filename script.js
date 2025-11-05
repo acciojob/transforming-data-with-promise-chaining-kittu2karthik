05ef574fd3b3709aa4a253cec5e3155183d2c2d0
kittu2karthik <71444538+kittu2karthik@users.noreply.github.com>
2025-11-05 18:45:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,29 +10,25 @@ function delay(time) {
 async function print(num) {
   output.innerHTML = '';
   await delay(2);
-  const p1 = document.createElement('p');
-  p1.textContent = `Result: ${num}`;
-  output.appendChild(p1);
-  await delay(3);
+  const p = document.createElement('p');
+  p.textContent = `Result: ${num}`;
+  output.appendChild(p);
+	
+  await delay(1);
   num *= 2;
-  const p2 = document.createElement('p');
-  p2.textContent = `Result: ${num}`;
-  output.appendChild(p2);
-  await delay(4);
+  p.textContent = `Result: ${num}`;
+	
+  await delay(1);
   num -= 3;
-  const p3 = document.createElement('p');
-  p3.textContent = `Result: ${num}`;
-  output.appendChild(p3);
-  await delay(5);
+  p.textContent = `Result: ${num}`;
+
+  await delay(1);
   num /= 2;
-  const p4 = document.createElement('p');
-  p4.textContent = `Result: ${num}`;
-  output.appendChild(p4);
-  await delay(6);
+  p.textContent = `Result: ${num}`;
+
+  await delay(1);
   num += 10;
-  const p5 = document.createElement('p');
-  p5.textContent = `Final Result: ${num}`; // Changed to "Final Result"
-  output.appendChild(p5);
+  p.textContent = `Final Result: ${num}`; 
 }
 
 btn.addEventListener('click', () => {
